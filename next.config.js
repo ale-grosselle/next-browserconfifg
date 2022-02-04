d1b855b4aaaef2071fc84ccc2cfa23e8541cb4af
Alessandro Grosselle <alessandro.grosselle@adevinta.com>
2022-02-04 14:54:16 +0100

    First commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-const BASE_PREFIX_FOR_APP = '/admin';
 module.exports = {
-  assetPrefix: BASE_PREFIX_FOR_APP,
+
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,19 @@ import React from "react";
 
 
 const callAll = async function () {
+  console.log("OK HELLO!");
+}
 
+class ThisIsAClass  {
+  constructor() {
+    console.log("HELLO FROM AN INSTANCE!")
+  }
 }
 
 const Index = () => {
   React.useEffect(() => {
     callAll();
+    new ThisIsAClass();
   }, [])
   return (
     <div>
